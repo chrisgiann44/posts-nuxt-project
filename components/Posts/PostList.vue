ffc2333c2930cbb16bf421fd8792781ac161cc51
Christos Giannouris <chris1giann1@gmail.com>
2019-10-30 16:33:13 +0100

    edit post

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -2,24 +2,28 @@
     <section class="post-list">
         <PostPreview
             id="1"
+            :is-admin="isAdmin"
             title="Hello there!"
             previewText="This is a post"
             thumbnail="https://tradeandinvest.wales/sites/default/files/styles/standard_75_desktop_retina/public/tech_sector_image_1428x952px_2.jpg?itok=FpJLzqWT"
         />
         <PostPreview
             id="2"
+            :is-admin="isAdmin"
             title="Hello there! 2"
             previewText="This is a post 2"
             thumbnail="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2F1160986680%2F960x0.jpg%3Ffit%3Dscale"
         />
         <PostPreview
             id="3"
+            :is-admin="isAdmin"
             title="Hello there! 3"
             previewText="This is a post 3"
             thumbnail="https://www.bsr.org/images/heroes/tech-against-trafficking-hero.jpg"
         />
         <PostPreview
             id="4"
+            :is-admin="isAdmin"
             title="Hello there! 4"
             previewText="This is a post 4"
             thumbnail="https://www.fairobserver.com/wp-content/uploads/2019/08/Tech-news.jpg"
@@ -33,6 +37,12 @@ import PostPreview from "@/components/Posts/PostPreview";
 export default {
     components: {
         PostPreview
+    },
+    props: {
+        isAdmin: {
+            type: Boolean,
+            default: false
+        }
     }
 };
 </script>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost" />
+        </section>
+    </div>
+</template>
+
+<script>
+import AdminPostForm from "@/components/Admin/AdminPostForm";
+export default {
+    components: {
+        AdminPostForm
+    },
+    data() {
+        return {
+            loadedPost: {
+                author: "Christos",
+                title: "Amazing",
+                content: "i am amazing",
+                thumbnail:
+                    "https://www.greeka.com/village_beach/photos/232/town-top-1-1280.jpg"
+            }
+        };
+    }
+};
+</script>
+
+<style scoped>
+.update-form {
+    width: 90%;
+    margin: 20px auto;
+}
+@media (min-width: 768px) {
+    .update-form {
+        width: 500px;
+    }
+}
+</style>
